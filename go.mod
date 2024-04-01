7be8ea03840797fe7d249f46a5cc69627f072866
cnderrauber <zengjie9004@gmail.com>
2024-04-01 15:05:31 +0800

    pion/datachannel benchmark tool

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/cnderrauber/sctpbenchmark
+
+go 1.22.1
+
+require (
+	github.com/pion/logging v0.2.2
+	github.com/pion/sctp v1.8.13
+	github.com/urfave/cli/v2 v2.27.1
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/pion/datachannel v1.5.6
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
---
+++ main.go
@@ -0,0 +1,249 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/pion/datachannel"
+	"github.com/pion/logging"
+	"github.com/pion/sctp"
+	"github.com/urfave/cli/v2"
+)
+
+const reportInterval = 10 * time.Second
+
+func main() {
+	app := &cli.App{
+		Name: "sctp benchmark",
+		Commands: []*cli.Command{
+			{
+				Name:    "server",
+				Aliases: []string{"s"},
+				Usage:   "start a server to receive data",
+				Action:  server,
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "addr",
+						Usage: "address to listen on",
+						Value: ":9901",
+					},
+					&cli.DurationFlag{
+						Name:  "duration",
+						Usage: "duration of test, default 1m",
+						Value: time.Minute,
+					},
+				},
+			},
+			{
+				Name:    "client",
+				Aliases: []string{"c"},
+				Usage:   "start a client to send data",
+				Action:  client,
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:  "rate",
+						Usage: "rate in MB/s of sending data",
+					},
+					&cli.IntFlag{
+						Name:  "size",
+						Usage: "size in Byte of message, default 1024 Byte",
+						Value: 1024,
+					},
+					&cli.DurationFlag{
+						Name:  "duration",
+						Usage: "duration of test, default 1m",
+						Value: time.Minute,
+					},
+					&cli.StringFlag{
+						Name:  "addr",
+						Usage: "address to connect to",
+						Value: "localhost:9901",
+					},
+					&cli.BoolFlag{
+						Name:  "loss",
+						Usage: "lossable channel",
+						Value: false,
+					},
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func server(c *cli.Context) error {
+	addr := c.String("addr")
+	duration := c.Duration("duration")
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return err
+	}
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		return err
+	}
+	fmt.Println("server listen on", addr)
+	defer conn.Close()
+
+	config := sctp.Config{
+		NetConn:       &UDPConn{UDPConn: conn},
+		LoggerFactory: logging.NewDefaultLoggerFactory(),
+	}
+	a, err := sctp.Server(config)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer a.Close()
+
+	dc, err := datachannel.Accept(a, &datachannel.Config{
+		LoggerFactory: logging.NewDefaultLoggerFactory(),
+	})
+	if err != nil {
+		return err
+	}
+	defer dc.Close()
+
+	fmt.Println("accepted a channel", dc.Label)
+
+	buf := make([]byte, 65536)
+	var totalBytes int64
+	start := time.Now()
+	lastReport := start
+	dc.SetReadDeadline(time.Now().Add(duration + time.Second))
+	for {
+		n, err := dc.Read(buf)
+		if err != nil {
+			break
+		}
+
+		totalBytes += int64(n)
+		if time.Since(lastReport) > reportInterval {
+			elapsed := time.Since(start)
+			lastReport = time.Now()
+			rate := float64(totalBytes) / 1024 / 1024 / elapsed.Seconds()
+			fmt.Printf("received %.2f MBytes in %.1fs, rate %.2f MB/s\n", float64(totalBytes)/1024/1024, elapsed.Seconds(), rate)
+		}
+	}
+
+	elapsed := time.Since(start)
+	rate := float64(totalBytes) / 1024 / 1024 / elapsed.Seconds()
+	fmt.Printf("received %.2f MBytes in %.1fs, rate %.2f MB/s\n", float64(totalBytes)/1024/1024, elapsed.Seconds(), rate)
+	return nil
+}
+
+func client(c *cli.Context) error {
+	addr := c.String("addr")
+	reliable := !c.Bool("loss")
+	rate := c.Int("rate")
+	msgSize := c.Int("size")
+	duration := c.Duration("duration")
+	conn, err := net.Dial("udp", addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	fmt.Println("client connect to", addr)
+
+	config := sctp.Config{
+		NetConn:       conn,
+		LoggerFactory: logging.NewDefaultLoggerFactory(),
+	}
+
+	a, err := sctp.Client(config)
+	if err != nil {
+		return err
+	}
+	fmt.Println("scpt client connected")
+
+	defer a.Close()
+
+	var dcConfig datachannel.Config
+	if reliable {
+		dcConfig = datachannel.Config{
+			ChannelType:   datachannel.ChannelTypeReliable,
+			Negotiated:    false,
+			Label:         "reliable",
+			LoggerFactory: logging.NewDefaultLoggerFactory(),
+		}
+	} else {
+		dcConfig = datachannel.Config{
+			ChannelType:   datachannel.ChannelTypePartialReliableRexmitUnordered,
+			Negotiated:    false,
+			Label:         "lossable",
+			LoggerFactory: logging.NewDefaultLoggerFactory(),
+		}
+	}
+	dc, err := datachannel.Dial(a, 1, &dcConfig)
+	if err != nil {
+		return err
+	}
+	defer dc.Close()
+
+	interval := time.Second * time.Duration(msgSize) / time.Duration(rate*1024*1024)
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	data := make([]byte, msgSize)
+	var totalBytes int64
+	start := time.Now()
+	lastReport := start
+	for range ticker.C {
+		n, err := dc.Write(data)
+		if err != nil {
+			return err
+		}
+
+		totalBytes += int64(n)
+		if time.Since(start) > duration {
+			break
+		}
+
+		if time.Since(lastReport) > reportInterval {
+			elapsed := time.Since(start)
+			lastReport = time.Now()
+			rate := float64(totalBytes) / 1024 / 1024 / elapsed.Seconds()
+			fmt.Printf("sent %.2f Mbytes in %.1fs, rate %.2f MB/s\n", float64(totalBytes)/1024/1024, elapsed.Seconds(), rate)
+		}
+	}
+
+	sentRate := float64(totalBytes) / 1024 / 1024 / time.Since(start).Seconds()
+	fmt.Printf("sent %.2f Mbytes in %.1fs, rate(target/actual) (%d/%.2f) MB/s\n", float64(totalBytes)/1024/1024, time.Since(start).Seconds(), rate, sentRate)
+
+	return nil
+}
+
+type UDPConn struct {
+	*net.UDPConn
+	lock  sync.RWMutex
+	raddr net.Addr
+}
+
+func (c *UDPConn) Read(b []byte) (n int, err error) {
+	var addr net.Addr
+	n, addr, err = c.UDPConn.ReadFrom(b)
+	if err == nil {
+		c.lock.Lock()
+		if c.raddr == nil {
+			c.raddr = addr
+		}
+		c.lock.Unlock()
+	}
+	return
+}
+
+func (c *UDPConn) Write(b []byte) (n int, err error) {
+	c.lock.RLock()
+	addr := c.raddr
+	c.lock.RUnlock()
+	if addr == nil {
+		return 0, errors.New("remote address not set")
+	}
+	return c.UDPConn.WriteTo(b, addr)
+}
